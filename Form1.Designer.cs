9d207a9f16b8d656a2514a2b9945eaf1a5198ec7
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2026-04-05 14:34:00 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WinSerialMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Bisque;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(93, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(134, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Port";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(233, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(295, 189);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 266);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(296, 76);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightBlue;
+            this.button3.Location = new System.Drawing.Point(234, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Send";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(13, 348);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.Text = "Speed connect";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Bisque;
+            this.button4.Location = new System.Drawing.Point(232, 237);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Bisque;
+            this.button5.Location = new System.Drawing.Point(151, 237);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Save";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 242);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(49, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Date";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(321, 381);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WinSerial Monitor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinSerialMonitor
+{
+    public partial class Form1 : Form
+    {
+        private bool isConnected = false;
+        private StringBuilder receivedData = new StringBuilder();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCustomComponents();
+        }
+
+        private void InitializeCustomComponents()
+        {
+            // Заполнение comboBox2 скоростями
+            comboBox2.Items.AddRange(new object[] {
+                "300", "1200", "2400", "4800", "9600", "19200",
+                "38400", "57600", "115200", "230400", "460800", "921600"
+            });
+            comboBox2.SelectedItem = "9600"; // Скорость по умолчанию
+
+            // Настройка serialPort
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+            serialPort1.NewLine = "\n"; // Устанавливаем символ новой строки
+            serialPort1.Encoding = Encoding.UTF8; // Явно указываем кодировку
+            serialPort1.ReadTimeout = 1000;
+            serialPort1.WriteTimeout = 1000;
+            serialPort1.DtrEnable = true; // Включаем DTR для Arduino
+            serialPort1.RtsEnable = true; // Включаем RTS для Arduino
+
+            // Обновление списка портов при запуске
+            UpdatePortList();
+
+            // Установка начального состояния UI (неактивное)
+            SetUIConnectedState(false);
+        }
+
+        private void UpdatePortList()
+        {
+            try
+            {
+                string[] ports = SerialPort.GetPortNames();
+                comboBox1.Items.Clear();
+                comboBox1.Items.AddRange(ports);
+                if (comboBox1.Items.Count > 0)
+                    comboBox1.SelectedIndex = 0;
+                else
+                    comboBox1.Text = "Нет доступных портов";
+            }
+            catch (Exception ex)
+            {
+                AddMessageToLog($"Ошибка при обновлении портов: {ex.Message}", true);
+            }
+        }
+
+        private StringBuilder receiveBuffer = new StringBuilder();
+
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                // Читаем все доступные данные
+                string data = serialPort1.ReadExisting();
+
+                if (!string.IsNullOrEmpty(data))
+                {
+                    lock (receiveBuffer)
+                    {
+                        receiveBuffer.Append(data);
+
+                        // Проверяем, есть ли полные строки (с разделителем \n)
+                        string bufferContent = receiveBuffer.ToString();
+                        int newlineIndex;
+
+                        while ((newlineIndex = bufferContent.IndexOf('\n')) >= 0)
+                        {
+                            // Извлекаем полную строку
+                            string completeLine = bufferContent.Substring(0, newlineIndex);
+                            receiveBuffer.Remove(0, newlineIndex + 1);
+
+                            // Убираем символ \r если есть
+                            completeLine = completeLine.TrimEnd('\r');
+
+                            if (!string.IsNullOrEmpty(completeLine))
+                            {
+                                string line = completeLine;
+                                this.Invoke(new Action(() =>
+                                {
+                                    AddMessageToLog(line, false);
+                                }));
+                            }
+
+                            bufferContent = receiveBuffer.ToString();
+                        }
+                    }
+                }
+            }
+            catch (TimeoutException)
+            {
+                // Игнорируем таймаут
+            }
+            catch (Exception ex)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    AddMessageToLog($"Ошибка при получении данных: {ex.Message}", true);
+                }));
+            }
+        }
+
+        private void AddMessageToLog(string message, bool isError)
+        {
+            // Получаем состояние чек-бокса (показывать время или нет)
+            bool showTimestamp = checkBox1.Checked;
+
+            // Разделяем сообщение на строки для лучшего форматирования
+            string[] lines = message.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+            foreach (string line in lines)
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    string formattedMessage;
+
+                    if (showTimestamp)
+                    {
+                        // С отметкой времени
+                        string timestamp = DateTime.Now.ToString("HH:mm:ss.fff");
+                        formattedMessage = $"[{timestamp}] {line}";
+
+                        if (isError)
+                            formattedMessage = $"[{timestamp}] [ОШИБКА] {line}";
+                    }
+                    else
+                    {
+                        // Без отметки времени
+                        formattedMessage = line;
+
+                        if (isError)
+                            formattedMessage = $"[ОШИБКА] {line}";
+                    }
+
+                    textBox1.AppendText(formattedMessage + Environment.NewLine);
+                }
+            }
+
+            // Прокрутка в конец
+            textBox1.ScrollToCaret();
+        }
+
+        private void SetUIConnectedState(bool connected)
+        {
+            // Кнопки
+            button3.Enabled = connected;  // Send
+            button4.Enabled = connected;  // Clear
+            button5.Enabled = connected;  // Save
+
+            // Текстовые поля
+            textBox2.Enabled = connected; // Поле ввода сообщений
+            textBox2.BackColor = connected ? SystemColors.Window : SystemColors.Control;
+
+            textBox1.Enabled = connected; // Поле ввода сообщений
+            textBox1.BackColor = connected ? SystemColors.Window : SystemColors.Control;
+
+            // Чек-бокс (можно оставить активным всегда для изменения формата лога)
+            checkBox1.Enabled = connected;
+
+            // Визуальный индикатор
+            if (connected)
+            {
+            }
+            else
+            {
+                textBox2.Text = "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdatePortList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!isConnected)
+                {
+                    // Проверка выбран ли порт
+                    if (comboBox1.SelectedItem == null || comboBox1.Text == "Нет доступных портов")
+                    {
+                        MessageBox.Show("Пожалуйста, выберите доступный порт!", "Ошибка",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    // Настройка параметров порта
+                    serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                    serialPort1.BaudRate = int.Parse(comboBox2.SelectedItem.ToString());
+                    serialPort1.DataBits = 8;
+                    serialPort1.StopBits = StopBits.One;
+                    serialPort1.Parity = Parity.None;
+                    serialPort1.Handshake = Handshake.None;
+
+                    // Настройки для Arduino
+                    serialPort1.DtrEnable = true;  // Сброс Arduino при подключении
+                    serialPort1.RtsEnable = true;
+
+                    // Открытие порта
+                    serialPort1.Open();
+
+                    // Даем время Arduino на перезагрузку (если DTR включен)
+                    if (serialPort1.DtrEnable)
+                        System.Threading.Thread.Sleep(2000);
+
+                    isConnected = true;
+
+                    button2.Text = "Disconnect";
+                    button2.BackColor = Color.LightCoral;
+                    comboBox1.Enabled = false;
+                    comboBox2.Enabled = false;
+                    button1.Enabled = false;
+
+                    // Активация UI элементов
+                    SetUIConnectedState(true);
+
+                    AddMessageToLog($"Подключено к {serialPort1.PortName} на скорости {serialPort1.BaudRate} бод", false);
+                }
+                else
+                {
+                    // Закрытие порта
+                    if (serialPort1.IsOpen)
+                    {
+                        serialPort1.Close();
+                    }
+                    isConnected = false;
+
+                    button2.Text = "Connect";
+                    button2.BackColor = SystemColors.Control;
+                    comboBox1.Enabled = true;
+                    comboBox2.Enabled = true;
+                    button1.Enabled = true;
+
+                    // Деактивация UI элементов
+                    SetUIConnectedState(false);
+
+                    AddMessageToLog($"Отключено от {serialPort1.PortName}", false);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Нет доступа к порту. Возможно, порт уже используется другим приложением.",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при подключении: {ex.Message}", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                AddMessageToLog($"Ошибка подключения: {ex.Message}", true);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                MessageBox.Show("Сначала подключитесь к устройству!", "Предупреждение",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string messageToSend = textBox2.Text.Trim();
+
+            if (string.IsNullOrEmpty(messageToSend))
+            {
+                MessageBox.Show("Введите сообщение для отправки!", "Предупреждение",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                // Отправка сообщения с символом новой строки
+                serialPort1.Write(messageToSend + "\n");
+
+                // Добавление отправленного сообщения в лог
+                AddMessageToLog($"Отправлено: {messageToSend}", false);
+
+                // Очистка поля ввода
+                textBox2.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при отправке: {ex.Message}", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                AddMessageToLog($"Ошибка отправки: {ex.Message}", true);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Нет данных для сохранения!", "Предупреждение",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.DefaultExt = "txt";
+            saveFileDialog.FileName = $"serial_log_{DateTime.Now:yyyyMMdd_HHmmss}.txt";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    File.WriteAllText(saveFileDialog.FileName, textBox1.Text, Encoding.UTF8);
+                    MessageBox.Show($"Данные успешно сохранены в файл:\n{saveFileDialog.FileName}",
+                        "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при сохранении: {ex.Message}", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Отправка сообщения по нажатию Enter (только если соединение активно)
+            if (e.KeyChar == (char)Keys.Enter && isConnected)
+            {
+                e.Handled = true;
+                button3_Click(sender, e);
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // Закрытие порта при закрытии приложения
+            if (isConnected && serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Close();
+                }
+                catch (Exception ex)
+                {
+                    // Игнорируем ошибки при закрытии
+                    System.Diagnostics.Debug.WriteLine($"Error closing port: {ex.Message}");
+                }
+            }
+            base.OnFormClosing(e);
+        }
+    }
+}
